5d0c7afb0084df7d3627dc4918307928c68b63d6
Arpit Tyagi <arpittyagi17551@gmail.com>
2025-12-26 12:55:25 +0530

    updated conf

--- seniorsays/src/conf/conf.js
+++ seniorsays/src/conf/conf.js
@@ -6,7 +6,7 @@ const conf = {
 
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE__DATABASE_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
